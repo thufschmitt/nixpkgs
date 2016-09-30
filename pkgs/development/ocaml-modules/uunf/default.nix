49567405b2829b5181dc5ba79005e72c07b9a2ba
Théophane Hufschmitt <theophane.hufschmitt@polytechnique.org>
2016-09-30 17:35:01 +0200

    ocamlPackages.uunf : add ocamlbuild to buildinputs

--- pkgs/development/ocaml-modules/uunf/default.nix
+++ pkgs/development/ocaml-modules/uunf/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, ocaml, findlib, opam}:
+{stdenv, fetchurl, ocaml, findlib, opam, ocamlbuild }:
 let
   pname = "uunf";
   version = "0.9.3";
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
     sha256 = "16cgjy1m0m61srv1pmlc3gr0y40kd4724clvpagdnz68raz4zmn0";
   };
 
-  buildInputs = [ ocaml findlib opam ];
+  buildInputs = [ ocaml findlib opam ocamlbuild ];
 
   createFindlibDestdir = true;
 
