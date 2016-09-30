26f2ccbc7474cf997d28b351cfc028ceb6d7e20d
Théophane Hufschmitt <theophane.hufschmitt@polytechnique.org>
2016-09-30 17:52:48 +0200

    all_ocaml : Parametrize on ocaml package set

--- all_ocaml.nix
+++ all_ocaml.nix
@@ -1,14 +1,7 @@
-{ nixpkgs ? ./. }:
-with import nixpkgs { config = { allowBroken = true; }; };
-# {
-    lib.filterAttrs
-        (_: val: val.src or null != null && !(val.meta.broken or false))
-        ocamlPackages_4_03
-  # ocaml_packages =
-  #   buildEnv {
-  #   name = "all_ocaml";
-  #   paths = lib.mapAttrsToList (name: value: value) (lib.filterAttrs
-  #       (_: val: val.src or null != null && !(val.meta.broken or false))
-  #   ocamlPackages_4_03);
-  #   };
-# }
+{ nixpkgs ? ./., ocamlPath ? "ocamlPackages_4_03" }:
+let pkgs = import nixpkgs { config = { allowBroken = true; }; }; in
+
+with pkgs;
+  lib.filterAttrs
+    (_: val: val.src or null != null && !(val.meta.broken or false))
+    pkgs."${ocamlPath}"
