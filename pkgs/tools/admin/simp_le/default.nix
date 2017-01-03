4289f551e2f1d83964661ef092763a6055199bd8
regnat <rg@regnat.ovh>
2017-01-03 11:19:01 +0100

    Merge branch 'nixos-unstable' of https://github.com/NixOS/nixpkgs-channels into current

--- pkgs/tools/admin/simp_le/default.nix
+++ pkgs/tools/admin/simp_le/default.nix
@@ -11,17 +11,6 @@ pythonPackages.buildPythonApplication rec {
     sha256 = "04dr8lvcpi797722lsy06nxhlihrxdqgdy187pg3hl1ki2iq3ixx";
   };
 
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/kuba/simp_le/commit/4bc788fdd611c4118c3f86b5f546779723aca5a7.patch";
-      sha256 = "0036p11qn3plydv5s5z6i28r6ihy1ipjl0y8la0izpkiq273byfc";
-    })
-    (fetchpatch {
-      url = "https://github.com/kuba/simp_le/commit/9ec7efe593cadb46348dc6924c1e6a31f0f9e636.patch";
-      sha256 = "0n3m94n14y9c42185ly47d061g6awc8vb8xs9abffaigxv59k06j";
-    })
-  ];
-
   propagatedBuildInputs = with pythonPackages; [ acme ];
 
   meta = with stdenv.lib; {
